3f79a34ecdb7fedb33c1dce74687b606eea150fd
장교준 <wkddn2003@gmail.com>
2025-02-04 15:11:50 +0900

    컬렉션 프레임워크

--- Exercise/Ch11/Ex_11_5.java
+++ Exercise/Ch11/Ex_11_5.java
@@ -3,7 +3,7 @@ package me.Gyojun.practice.Exercise.Ch11;
 import java.util.*;
 
 
-class Student0 implements Comparable<Student> {
+class Student0 implements Comparable<Student0> {
     String name;
     int ban;
     int no;
@@ -39,7 +39,7 @@ class Student0 implements Comparable<Student> {
 
     @Override
     public String toString() {
-        return "Student{" +
+        return "Student0{" +
                 "name='" + name + '\'' +
                 ", ban=" + ban +
                 ", no=" + no +
@@ -51,31 +51,30 @@ class Student0 implements Comparable<Student> {
 
 
     @Override
-    public int compareTo(Student o) {
+    public int compareTo(Student0 o) {
         return this.name.compareTo(o.name);
 //    }
     }
-
-
-    public class Ex_11_5 {
-        public static void main(String[] args) {
-
-            ArrayList list = new ArrayList();
-            list.add(new Student("홍길동", 1, 1, 100, 100, 100));
-            list.add(new Student("남궁성", 1, 2, 90, 70, 80));
-            list.add(new Student("김자바", 1, 3, 80, 80, 90));
-            list.add(new Student("이자바", 1, 4, 70, 90, 70));
-            list.add(new Student("안자바", 1, 5, 60, 100, 80));
-
-
-            Collections.sort(list);
-            Iterator it = list.iterator();
-
-            while (it.hasNext())
-                System.out.println(it.next());
-        }
-
-    }
+    
 }
 
 
+public class Ex_11_5 {
+    public static void main(String[] args) {
+
+        ArrayList list = new ArrayList();
+        list.add(new Student0("홍길동", 1, 1, 100, 100, 100));
+        list.add(new Student0("남궁성", 1, 2, 90, 70, 80));
+        list.add(new Student0("김자바", 1, 3, 80, 80, 90));
+        list.add(new Student0("이자바", 1, 4, 70, 90, 70));
+        list.add(new Student0("안자바", 1, 5, 60, 100, 80));
+
+
+        Collections.sort(list);
+        Iterator it = list.iterator();
+
+        while (it.hasNext())
+            System.out.println(it.next());
+    }
+
+}
--- Exercise/Ch11/Ex_11_6.java
+++ Exercise/Ch11/Ex_11_6.java
@@ -3,7 +3,7 @@ package me.Gyojun.practice.Exercise.Ch11;
 import java.util.*;
 
 
-class Student implements Comparable<Student> {
+class Student1 implements Comparable<Student1> {
     String name;
     int ban;
     int no;
@@ -12,9 +12,9 @@ class Student implements Comparable<Student> {
     int math;
 
 
-    Student(){}
+    Student1(){}
 
-    Student(   String name, int ban, int no, int kor, int eng, int math) {
+    Student1(   String name, int ban, int no, int kor, int eng, int math) {
         this.name = name;
         this.ban = ban;
         this.no = no;
@@ -39,7 +39,7 @@ class Student implements Comparable<Student> {
 
     @Override
     public String toString() {
-        return "Student{" +
+        return "Student1{" +
                 "name='" + name + '\'' +
                 ", ban=" + ban +
                 ", no=" + no +
@@ -51,7 +51,7 @@ class Student implements Comparable<Student> {
 
 
     @Override
-    public int compareTo(Student o) {
+    public int compareTo(Student1 o) {
         return this.name.compareTo(o.name);
     }
 }
@@ -59,15 +59,15 @@ class Student implements Comparable<Student> {
 
 public class Ex_11_6 {
 
-    static int getGroupCount(TreeSet<Student> tset, int from, int to) {
+    static int getGroupCount(TreeSet<Student1> tset, int from, int to) {
         int count = 0;
-        Iterator<Student> it = tset.iterator();
+        Iterator<Student1> it = tset.iterator();
 
 
         // while문 it가 가지고 있을 때
         while (it.hasNext()) {
             float average = it.next().getAverage();
-            // it.next() -> Student => it.next().getAverage()가 60이상 70미만일 때
+            // it.next() -> Student1 => it.next().getAverage()가 60이상 70미만일 때
             if (average >= from && average <= to)
                 count++;
         }
@@ -79,13 +79,13 @@ public class Ex_11_6 {
 
 
         TreeSet set = new TreeSet(new Comparator() {
-//            TreeSet set = new TreeSet(new Comparator<Student>() {
+//            TreeSet set = new TreeSet(new Comparator<Student1>() {
             @Override
             public int compare(Object o1, Object o2) {
-                if(o1 instanceof Student && o2 instanceof Student) {
+                if(o1 instanceof Student1 && o2 instanceof Student1) {
 
-                        Student tmp = (Student) o1;
-                        Student tmp2 = (Student) o2;        //
+                    Student1 tmp = (Student1) o1;
+                    Student1 tmp2 = (Student1) o2;        //
 
                         return  tmp2.no - tmp.no;
 
@@ -96,10 +96,10 @@ public class Ex_11_6 {
         });
 
 
-        set.add(new Student("홍길동",1,1,100,100,100));
-        set.add(new Student("남궁성",1,2,90,70,80));
-        set.add(new Student("김자바",1,3,80,80,90));
-        set.add(new Student("이자바",1,4,70,90,70));
+        set.add(new Student1("홍길동",1,1,100,100,100));
+        set.add(new Student1("남궁성",1,2,90,70,80));
+        set.add(new Student1("김자바",1,3,80,80,90));
+        set.add(new Student1("이자바",1,4,70,90,70));
 
 
 
